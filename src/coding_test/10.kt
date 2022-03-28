54f62769e707335e4006e31c39540e8441e1f283
Lkh-dev <Lkh10195@gmail.com>
2022-03-28 12:46:41 +0900

    3/28 학습내용

--- src/coding_test/10.kt
+++ src/coding_test/10.kt
@@ -1,3 +1,5 @@
+import java.util.*
+
 // 가장 가까운 사람의 이름을 알려주는 함수를 완성하세요
 // 거리는 음수가 아닙니다 / 같은 거리는 없습니다.
 
@@ -7,14 +9,24 @@ fun main(){
     println(result1)
     // 찬혁
 
-    val result2 = solution("지영 : 3.7, 민영  : 1.2 , 해피 : 2.7, 퍼니 : 3")
+    val result2 = solution("지영 : 3.7, 민영 : 1.2, 해피 : 2.7, 퍼니 : 3")
     println(result2)
     // 민영
 
 }
 
 
-fun solution(str: String) : String{
+fun solution(str: String) : String {
+    var resMap = mutableMapOf<String, Double>()
+    var info = str.split(", ")
+    for (i in info) {
+        var name = i.split(" : ")[0]
+        var dist = i.split(" : ")[1]
+        resMap.put(name, dist.toDouble())
+
+    }
+
+    var least = Collections.min(resMap.values)
+    return least.toString()
 
 }
-
