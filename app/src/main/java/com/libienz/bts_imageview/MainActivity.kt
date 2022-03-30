3fd15bc98d7b7e087b308af5609b6b3105dd94a0
Lkh-Dev <Leekh10195@gmail.com>
2022-03-30 11:23:58 +0900

    3/30 학습내용

--- app/src/main/java/com/libienz/bts_imageview/MainActivity.kt
+++ app/src/main/java/com/libienz/bts_imageview/MainActivity.kt
@@ -2,10 +2,17 @@ package com.libienz.bts_imageview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val bts_1 = findViewById<ImageView>(R.id.BtsImage_1)
+        bts_1.setOnClickListener {
+            Toast.makeText(this, "1번사진 클릭 완료", Toast.LENGTH_SHORT).show()
+        }
     }
 }
