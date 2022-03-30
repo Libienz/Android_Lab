33754b308af0c2e686ab6275dc3f8c78df74b3b3
Lkh-Dev <Leekh10195@gmail.com>
2022-03-30 12:09:11 +0900

    3/30 학습내용

---
+++ app/src/main/java/com/libienz/twice_imageview/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.libienz.twice_imageview
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
