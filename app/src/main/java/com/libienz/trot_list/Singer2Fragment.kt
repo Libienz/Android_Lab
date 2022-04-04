ea8ed71cba971bbe761d47ec9afe3572d660353f
Lkh-dev <Leekh10195@gmail.com>
2022-04-04 10:39:56 +0900

    4/4 학습내용 리사이클러 작업

--- app/src/main/java/com/libienz/trot_list/Singer2Fragment.kt
+++ app/src/main/java/com/libienz/trot_list/Singer2Fragment.kt
@@ -7,6 +7,8 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 
 class Singer2Fragment : Fragment() {
@@ -23,14 +25,40 @@ class Singer2Fragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
 
+
         val view = inflater.inflate(R.layout.fragment_singer2, container, false)
 
+        val rv = view.findViewById<RecyclerView>(R.id.Singer2Rv)
+        val items = mutableListOf<String>()
+        items.add("사랑은 늘 도망가")
+        items.add("별빛 같은 사랑")
+        items.add("사랑은 늘 도망가")
+        items.add("별빛 같은 사랑")
+        items.add("사랑은 늘 도망가")
+        items.add("별빛 같은 사랑")
+        items.add("사랑은 늘 도망가")
+        items.add("별빛 같은 사랑")
+        items.add("사랑은 늘 도망가")
+        items.add("별빛 같은 사랑")
+        items.add("사랑은 늘 도망가")
+        items.add("별빛 같은 사랑")
+        items.add("사랑은 늘 도망가")
+        items.add("별빛 같은 사랑")
+        items.add("사랑은 늘 도망가")
+        items.add("별빛 같은 사랑")
+
+        val rvAdapter = RVAdapter(items)
+        rv.adapter = rvAdapter
+
+        rv.layoutManager = LinearLayoutManager(context)
         view.findViewById<ImageView>(R.id.image1).setOnClickListener {
             findNavController().navigate(R.id.action_singer2Fragment2_to_singer1Fragment)
         }
         view.findViewById<ImageView>(R.id.image3).setOnClickListener {
             findNavController().navigate(R.id.action_singer2Fragment2_to_singer3Fragment)
         }
+
+
 
         return view
     }
--- app/src/main/java/com/libienz/trot_list/Singer3Fragment.kt
+++ app/src/main/java/com/libienz/trot_list/Singer3Fragment.kt
@@ -7,6 +7,8 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 
 class Singer3Fragment : Fragment() {
@@ -21,6 +23,60 @@ class Singer3Fragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         val view = inflater.inflate(R.layout.fragment_singer3, container, false)
+
+        val rv = view.findViewById<RecyclerView>(R.id.Singer3Rv)
+        val items : MutableList<String> = mutableListOf<String>()
+        items.add("진정인가요")
+        items.add("내마음의 사진")
+        items.add("가인이어라")
+        items.add("진정인가요")
+        items.add("내마음의 사진")
+        items.add("가인이어라")
+        items.add("진정인가요")
+        items.add("내마음의 사진")
+        items.add("가인이어라")
+        items.add("진정인가요")
+        items.add("내마음의 사진")
+        items.add("가인이어라")
+        items.add("진정인가요")
+        items.add("내마음의 사진")
+        items.add("가인이어라")
+        items.add("진정인가요")
+        items.add("내마음의 사진")
+        items.add("가인이어라")
+        items.add("진정인가요")
+        items.add("내마음의 사진")
+        items.add("가인이어라")
+        items.add("진정인가요")
+        items.add("내마음의 사진")
+        items.add("가인이어라")
+        items.add("진정인가요")
+        items.add("내마음의 사진")
+        items.add("가인이어라")
+        items.add("진정인가요")
+        items.add("내마음의 사진")
+        items.add("가인이어라")
+        items.add("진정인가요")
+        items.add("내마음의 사진")
+        items.add("가인이어라")
+        items.add("진정인가요")
+        items.add("내마음의 사진")
+        items.add("가인이어라")
+        items.add("진정인가요")
+        items.add("내마음의 사진")
+        items.add("가인이어라")
+        items.add("진정인가요")
+        items.add("내마음의 사진")
+        items.add("가인이어라")
+        items.add("진정인가요")
+        items.add("내마음의 사진")
+        items.add("가인이어라")
+
+
+        val rvAdapter = RVAdapter(items)
+        rv.adapter = rvAdapter
+
+        rv.layoutManager = LinearLayoutManager(context)
 
         view.findViewById<ImageView>(R.id.image2).setOnClickListener {
             findNavController().navigate(R.id.action_singer3Fragment_to_singer2Fragment)
