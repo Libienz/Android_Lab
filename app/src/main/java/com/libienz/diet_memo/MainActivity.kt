6a9d285fa4f5e72e65ed5d8ce9fcb40cbb70c53a
Lkh-dev <Leekh10195@gmail.com>
2022-04-05 23:07:05 +0900

    dialog and calender

--- app/src/main/java/com/libienz/diet_memo/MainActivity.kt
+++ app/src/main/java/com/libienz/diet_memo/MainActivity.kt
@@ -1,11 +1,49 @@
 package com.libienz.diet_memo
 
+import android.app.DatePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.widget.Button
+import android.widget.DatePicker
+import android.widget.ImageView
+import androidx.appcompat.app.AlertDialog
+import java.time.Month
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val writeBtn = findViewById<ImageView>(R.id.icon)
+
+        writeBtn.setOnClickListener {
+            Log.d("WRITE CLICK", "Clicked")
+            val mDialogView = LayoutInflater.from(this).inflate(R.layout.custom_dialog, null)
+            val mBuilder = AlertDialog.Builder(this)
+                .setView(mDialogView)
+                .setTitle("운동 메모 다이얼로그")
+
+            val mAlertDialog = mBuilder.show()
+            val dateSelectBtn = mAlertDialog.findViewById<Button>(R.id.date_select_btn)
+            dateSelectBtn!!.setOnClickListener {
+                val today = GregorianCalendar()
+                val year = today.get(Calendar.YEAR)
+                val month = today.get(Calendar.MONTH)
+                val date = today.get(Calendar.DATE)
+
+                val dlg = DatePickerDialog(this, object : DatePickerDialog.OnDateSetListener {
+                    override fun onDateSet(p0: DatePicker?, p1: Int, p2: Int, p3: Int) {
+
+                    }
+
+                },year,month,date)
+                dlg.show()
+            }
+        }
     }
 }
