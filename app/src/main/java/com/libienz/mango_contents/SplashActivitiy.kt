fffcfa5b8cb15950da5c640a13c3e3fc42fb44eb
Lkh-dev <Leekh10195@gmail.com>
2022-05-03 15:21:40 +0900

    Splash Activity Done

---
+++ app/src/main/java/com/libienz/mango_contents/SplashActivitiy.kt
@@ -0,0 +1,18 @@
+package com.libienz.mango_contents
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+
+class SplashActivitiy : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_activitiy)
+
+        Handler().postDelayed({
+            startActivity(Intent(this,MainActivity::class.java))
+            finish()
+        }, 3000)
+    }
+}
