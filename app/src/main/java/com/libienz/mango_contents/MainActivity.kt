6fd2d91fb42378e34cd125a080fa0bf974617f0e
Lkh-dev <Leekh10195@gmail.com>
2022-05-05 18:16:11 +0900

    Web View done

--- app/src/main/java/com/libienz/mango_contents/MainActivity.kt
+++ app/src/main/java/com/libienz/mango_contents/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.libienz.mango_contents
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -23,9 +25,16 @@ class MainActivity : AppCompatActivity() {
         )
         items.add(
             ContentsModel(
-                "https://www.mangoplate.com/restaurants/HrBaIZj2EXJH",
-                "https://mp-seoul-image-production-s3.mangoplate.com/411974/1157298_1602939485186_11752?fit=around|512:512&crop=512:512;*,*&output-format=jpg&output-quality=80"
-                , "경성초밥"
+                "https://www.mangoplate.com/restaurants/0my2hMzHlOYD",
+                "https://mp-seoul-image-production-s3.mangoplate.com/283682/1761156_1648810904086_3716?fit=around|512:512&crop=512:512;*,*&output-format=jpg&output-quality=80"
+                , "당도"
+            )
+        )
+        items.add(
+            ContentsModel(
+                "https://www.mangoplate.com/restaurants/L6LQdsD4NSLy",
+                "https://mp-seoul-image-production-s3.mangoplate.com/419195/720385_1600098040669_16153?fit=around|512:512&crop=512:512;*,*&output-format=jpg&output-quality=80"
+                , "시홍쓰"
             )
         )
         items.add(
@@ -35,10 +44,52 @@ class MainActivity : AppCompatActivity() {
                 , "경성초밥"
             )
         )
+        items.add(
+            ContentsModel(
+                "https://www.mangoplate.com/restaurants/0my2hMzHlOYD",
+                "https://mp-seoul-image-production-s3.mangoplate.com/283682/1761156_1648810904086_3716?fit=around|512:512&crop=512:512;*,*&output-format=jpg&output-quality=80"
+                , "당도"
+            )
+        )
+        items.add(
+            ContentsModel(
+                "https://www.mangoplate.com/restaurants/L6LQdsD4NSLy",
+                "https://mp-seoul-image-production-s3.mangoplate.com/419195/720385_1600098040669_16153?fit=around|512:512&crop=512:512;*,*&output-format=jpg&output-quality=80"
+                , "시홍쓰"
+            )
+        )
+        items.add(
+            ContentsModel(
+                "https://www.mangoplate.com/restaurants/HrBaIZj2EXJH",
+                "https://mp-seoul-image-production-s3.mangoplate.com/411974/1157298_1602939485186_11752?fit=around|512:512&crop=512:512;*,*&output-format=jpg&output-quality=80"
+                , "경성초밥"
+            )
+        )
+        items.add(
+            ContentsModel(
+                "https://www.mangoplate.com/restaurants/0my2hMzHlOYD",
+                "https://mp-seoul-image-production-s3.mangoplate.com/283682/1761156_1648810904086_3716?fit=around|512:512&crop=512:512;*,*&output-format=jpg&output-quality=80"
+                , "당도"
+            )
+        )
+        items.add(
+            ContentsModel(
+                "https://www.mangoplate.com/restaurants/L6LQdsD4NSLy",
+                "https://mp-seoul-image-production-s3.mangoplate.com/419195/720385_1600098040669_16153?fit=around|512:512&crop=512:512;*,*&output-format=jpg&output-quality=80"
+                , "시홍쓰"
+            )
+        )
         val recyclerView = findViewById<RecyclerView>(R.id.rv)
         val rvAdapter = RVAdapter(baseContext, items)
         recyclerView.adapter = rvAdapter
 
+        rvAdapter.itemClick = object : RVAdapter.ItemClick {
+            override fun onClick(view: View, position: Int) {
+                val intent = Intent(baseContext,ViewActivity::class.java)
+                intent.putExtra("url",items[position].url)
+                startActivity(intent)
+            }
+        }
         recyclerView.layoutManager  = GridLayoutManager(this,2)
     }
 }
